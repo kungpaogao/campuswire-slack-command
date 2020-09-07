fc21d473a75bf2229dd39b4130afecdfbc9c50b4
Andrew <kungpaogao@gmail.com>
2020-09-07 01:08:51 -0400

    Implement basic handling for returning link

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-const Router = require('./router')
+import qs from "qs";
+
+const Router = require("./router");
+const BASE_URL = "https://campuswire.com/c/G87498D9F/feed/";
 
 /**
  * Example of how router can be used in an application
  *  */
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
-function handler(request) {
-    const init = {
-        headers: { 'content-type': 'application/json' },
-    }
-    const body = JSON.stringify({ some: 'json' })
-    return new Response(body, init)
+async function handler(request) {
+  const init = {
+    headers: { "content-type": "application/json" },
+  };
+
+  const req = await request.text();
+  const params = qs.parse(req);
+  const num = params["text"].trim();
+
+  const body = JSON.stringify({
+    response_type: "in_channel",
+    text: `${BASE_URL}${num}`,
+  });
+  return new Response(body, init);
 }
 
 async function handleRequest(request) {
-    const r = new Router()
-    // Replace with the appropriate paths and handlers
-    r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('.*/foo', request => handler(request))
-    r.post('.*/foo.*', request => handler(request))
-    r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
+  const router = new Router();
 
-    r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
+  router.post("/campuswire", (request) => handler(request));
 
-    const resp = await r.route(request)
-    return resp
+  let response = await router.route(request);
+  if (!response) {
+    response = new Response("Not found", { status: 404 });
+  }
+  return response;
 }
